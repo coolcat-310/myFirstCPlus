d7981b097976962095be0cc9e285481af98ff898
navarretejuan <navarretejuan09@gmail.com>
2017-02-03 14:48:43 -0800

    here you go

--- main.cpp
+++ main.cpp
@@ -3,27 +3,45 @@
 using namespace std;
 // import std so that you print using cout instead std:: cout
 
+#include <iomanip>
+using std::setw;
+
+double getProduct(double arr1[], double arr2[]){
+    double localanswer = 0;
+    for(int i =0; i< sizeof(arr1); i++){
+        double clear = 0;
+        clear = arr1[i]*arr2[i];
+        localanswer += clear;
+    }
+
+    return localanswer;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl; //stdout is standard character output
-    // << is an insertion operator which indicates that what follows is inserted into std::cout
-//    std::cout << "I'm a C++ program";
 
-    int a, b;
-    auto result = a;
-    // auto cast the data type result equal to the data type of variable a
+    double arrayOne[3] = {1.23, 3.45, 6.00};
+    double arrayTwo[3] = {3.0, 6.0, 9.0};
 
-    a = 5;
-    b = 2;
-    a = a + 1;
-    result = a - b;
+    double answer = getProduct(arrayOne, arrayTwo);
 
-    cout << result;
-
-    string mystring;
-    mystring = "\nthis is the initial string content";
-    cout << mystring;
-    mystring = "\nThis is a different string content";
-    cout << mystring;
+    cout << answer <<::endl;
 
     return 0;
 }
+
+
+
+
+//    int n[ 10 ]; // n is an array of 10 integers
+//
+//    // initialize elements of array n to 0
+//    for ( int i = 0; i < 10; i++ ) {
+//        n[ i ] = i + 100; // set element at location i to i + 100
+//    }
+//
+//    cout << "Element" << setw( 13 ) << "Value" << endl;
+//
+//    // output each array element's value
+//    for ( int j = 0; j < 10; j++ ) {
+//        cout << setw( 7 )<< j << setw( 13 ) << n[ j ] << endl;
+//    }
