8dd08fa3d368e2a13a774f8cb0bfc4c6220a85fa
navarretejuan <navarretejuan09@gmail.com>
2017-02-03 14:52:05 -0800

    hey

--- main.cpp
+++ main.cpp
@@ -38,10 +38,12 @@ int main() {
 //    for ( int i = 0; i < 10; i++ ) {
 //        n[ i ] = i + 100; // set element at location i to i + 100
 //    }
-//
+/
 //    cout << "Element" << setw( 13 ) << "Value" << endl;
 //
 //    // output each array element's value
 //    for ( int j = 0; j < 10; j++ ) {
 //        cout << setw( 7 )<< j << setw( 13 ) << n[ j ] << endl;
 //    }
+
+
